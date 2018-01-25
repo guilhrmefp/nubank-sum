091c51c650685e68383a4ecbc703b05121633b73
Guilherme Fernandes Pereira <o.guifernandes@gmail.com>
2018-01-25 15:27:57 -0200

    Working version

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-insertCode = function () {
-  _TOTAL = 0
-  _TAB = document.querySelector('.md-tab-content:not(.ng-hide)')
+_TOTAL = 0
+_TAB = document.querySelector('.md-tab-content:not(.ng-hide)')
 
-  var totalTheme = '<li class="group ng-scope total-checked"><div class="label">Total selecionado</div><div class="amount positive ng-binding">R$0</div></li>'
-  _TAB.querySelector('.summary ul').insertAdjacentHTML('beforeend', totalTheme)
+var totalTheme = '<li class="group ng-scope total-checked"><div class="label">Total selecionado</div><div class="amount positive ng-binding">R$0</div></li>'
+_TAB.querySelector('.summary ul').insertAdjacentHTML('beforeend', totalTheme)
 
-  var bills = _TAB.querySelectorAll('.charges-list .charge')
+var bills = _TAB.querySelectorAll('.charges-list .charge')
 
-  bills.forEach(function(bill){
-    var amount = bill.querySelector('.amount').textContent.replace(',', '.')
+bills.forEach(function(bill){
+  var amount = bill.querySelector('.amount').textContent.replace(',', '.')
 
-    var input = '<input type="checkbox" value="' + amount + '" onClick="amountTotal(this)">'
-    var inputTheme = '<label class="bill-check">' + input + '<span class="bill-check__icon"></span></label>'
+  var input = '<input type="checkbox" value="' + amount + '" class="bill-input">'
+  var inputTheme = '<label class="bill-check">' + input + '<span class="bill-check__icon"></span></label>'
 
-    var description = bill.querySelector('.description')
-    description.insertAdjacentHTML('afterbegin', inputTheme)
-  })
-}
+  var description = bill.querySelector('.description')
+  description.insertAdjacentHTML('afterbegin', inputTheme)
+})
 
-amountTotal = function (el) {
+const amountTotal = function (el) {
   var amount = parseFloat(el.value)
 
   if (el.checked) {
@@ -27,5 +25,13 @@ amountTotal = function (el) {
     _TOTAL -= amount
   }
 
-  document.querySelector('.total-checked .amount').textContent = 'R$' + _TOTAL.toFixed(2)
+  document.querySelector('.total-checked .amount').textContent = _TOTAL.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 }
+
+// listens to clicks only inside bills div
+document.querySelector('.md-tab-content:not(.ng-hide)').addEventListener('click', function(e) {
+  let target = e.target
+  if (target.className === 'bill-input') {
+    amountTotal(target)
+  }
+}, false);
--- nu.js
+++ nu.js
@@ -1,16 +1,11 @@
-insertCode = function () {
-  var link = document.createElement("link");
-  link.href = "file:///C:/Users/Guilherme/Documents/nubank/main.css";
-  link.type = "text/css";
-  link.rel = "stylesheet";
-
-  var script = document.createElement('script');
-  script.setAttribute('src', 'file:///C:/Users/Guilherme/Documents/nubank/main.js');
-  script.setAttribute('type', 'text/javascript');
-
-  document.getElementsByTagName("head")[0].appendChild(link);
-  document.getElementsByTagName("head")[0].appendChild(script);
-
-alert('asdasd')
+const cssFile = {
+  file: 'main.css'
+}
+const jsFile = {
+  file: 'main.js'
 }
 
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript(jsFile)
+  chrome.tabs.insertCSS(cssFile)
+});
